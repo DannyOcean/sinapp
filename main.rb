123858fa23aa23e12a2cc9655f3462e7c7ff695e
DannyOcean <dk.winni@gmail.com>
2014-03-17 01:23:50 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/flash'
+require './lab_1_1'
+require './lab_1_2'
+require './lab_1_3'
+
+get '/' do
+  haml :index, layout: false
+end
+
+get '/lab1' do
+  haml :lab1
+end
+
+post '/lab1' do
+  @cipher = Cipher.new
+  if params[:action] == 'encrypt'
+    @cipher.encrypt(params[:phrase], params[:key])
+  else
+    @cipher.decrypt(params[:phrase], params[:key])
+  end
+  @result = @cipher.sub_phrase
+  haml :lab1, locals: { result: @result }
+end
+
+get '/lab2' do
+  haml :lab2
+end
+
+post '/lab2' do
+  @permutator = Permutator.new
+  if params[:action] == 'encrypt'
+    @permutator.encode(params[:phrase])
+    @result = @permutator.encoded_phrase
+  else
+    @permutator.decode(params[:phrase])
+    @result = @permutator.decoded_phrase
+  end
+  haml :lab2, locals: { result: @result }
+end
+
+get '/lab3' do
+  haml :lab3
+end
+
+post '/lab3' do
+  @gamma = Gamma.new
+  if params[:action] == 'encrypt'
+    @gamma.encrypt(params[:phrase], params[:key])
+    @result = @gamma.encrypted_phrase
+  else
+    @gamma.decrypt(params[:phrase], params[:key])
+    @result = @gamma.decrypted_phrase
+  end
+  haml :lab3, locals: { result: @result }    
+end
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This app combines three laboratory works on the subject "Informational Security". I think realization could be better, but I'm just learning :)
+
+This app can be useful actually! With the help of this app you can encrypt or decrypt some messages.. How about some private discussions of fb? ;)
